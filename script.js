2bbaba97d9a3a38c7572fbdbc5827ecbbd9af2a7
Insomnia2024 <siebertjannik59@gmail.com>
2024-09-11 16:48:20 +0200

    pull

---
+++ script.js
@@ -0,0 +1,23 @@
+let numberToGuess = Math.round(Math.random() * 100)
+let tries = 0;
+
+
+function guessTheNumber() {
+    alert("Es funktioniert!" + numberToGuess)
+
+    tries = tries + 1;
+    displayTries.innerHTML = 'Versuche: ' + tries;
+    if(numberToGuess == input.value) {
+        headline.innerHTML = 'Du hast gewonnen die Zahl war ' + numberToGuess;
+        const jsConfetti = new JSConfetti()
+        jsConfetti.addConfetti()
+    }
+    if(numberToGuess < input.value) {
+        headline.innerHTML = 'Deine Zahl ist zu Groß'
+    }
+    if(numberToGuess > input.value) {
+        headline.innerHTML = 'Deine Zahl ist zu klein'
+    }
+
+    input.value = '';
+}
